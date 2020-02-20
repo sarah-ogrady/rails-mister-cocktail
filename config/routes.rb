93b0c1f798b5700634638b61922a4e9a0af64dd2
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-02-20 16:19:55 +0000

    finished logic of app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: %i[index new create show] do
+    resources :doses, only: %i[new create]
+  end
+  resources :doses, only: [:destroy]
 end
