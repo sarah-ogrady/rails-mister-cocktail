dbbcae23bf72d4f03b7f602d24ef4cf894ee45a5
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-02-20 12:25:13 +0000

    finished the models

---
+++ app/models/dose.rb
@@ -0,0 +1,6 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+  validates :description, presence: true
+  validates :cocktail_id, uniqueness: { scope: :ingredient_id }
+end
