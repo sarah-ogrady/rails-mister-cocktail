1fcde1a11de54625e87a9c2f4663aaf0fc3681a2
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-08-13 10:49:35 +0100

    add destroy method

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -20,6 +20,12 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
   private
 
   def cocktail_params
